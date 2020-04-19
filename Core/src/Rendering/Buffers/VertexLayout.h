74ce37df75a00fc37f433289ae9f882acfaf5a56
rmdir-dev <stonsphilippe@gmail.com>
2020-04-19 15:16:00 +0200

    [VERTEX] add vertex layout.

---
+++ Core/src/Rendering/Buffers/VertexLayout.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "glm/glm.hpp"
+#include "Utils/Type.h"
+
+namespace Rendering
+{
+    struct VertexLayout 
+    {
+        /*
+        Positions X Y Z
+        Type FLOAT
+        Size 3 floats | glm::vec3
+        */
+        glm::vec3 position;
+
+        /*
+        Chanels Red Green Blue Alpha
+        Type FLOAT
+        Size 4 floats | glm::vec4
+        */
+        glm::vec4 color;
+
+        /*
+        Coor X Y
+        Type FLOAT
+        Size 2 floats | glm::vec2
+        */
+        glm::vec2 UVs;
+        
+        //glm::vec3 normals;
+    };
+}
