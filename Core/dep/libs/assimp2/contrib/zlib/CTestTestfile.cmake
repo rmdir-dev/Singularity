ca87444ca9000e1f944803d2482d1c71cce87555
rmdir-dev <stonsphilippe@gmail.com>
2020-04-21 14:54:08 +0200

    [ASSIMP] stable version of assimp

---
+++ Core/dep/libs/assimp2/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/FalconEngine/EH_Engine/Core/dep/libs/assimp/contrib/zlib
+# Build directory: D:/FalconEngine/EH_Engine/Core/dep/libs/assimp/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Core/dep/libs/assimp2/test/headercheck/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rmdir/TFE/SingularityEngine/Core/dep/libs/assimp2/test/headercheck
+# Build directory: /home/rmdir/TFE/SingularityEngine/Core/dep/libs/assimp2/test/headercheck
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
