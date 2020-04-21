e51fa6710d1dcaf927b656c0098c90da361064c3
rmdir-dev <stonsphilippe@gmail.com>
2020-04-21 16:27:08 +0200

    [ASSIMP]

--- Core/dep/libs/assimp2/revision.h
+++ Core/dep/libs/assimp2/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0xf7b4032
+#define GitVersion 0xc531faf
 #define GitBranch "master"
 
 #endif // ASSIMP_REVISION_H_INC
