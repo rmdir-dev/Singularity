cd2b4bb6d98b298ad99f3fa2b28ad5fd5d8f6b76
rmdir-dev <stonsphilippe@gmail.com>
2020-04-24 21:32:54 +0200

    [TEXTURE] define syntax corrections.

--- Core/src/Rendering/Texture/Texture.h
+++ Core/src/Rendering/Texture/Texture.h
@@ -2,9 +2,9 @@
 
 #include "Utils/Type.h"
 
-#define DIFFUSE     0x1; // 0000 0001
-#define SPECULAR    0x2; // 0000 0010
-#define NORMAL      0x4; // 0000 0100
+#define DIFFUSE     0x01 // 0000 0001
+#define SPECULAR    0x02 // 0000 0010
+#define NORMAL      0x04 // 0000 0100
 
 
 namespace Rendering
@@ -17,7 +17,7 @@ namespace Rendering
             byte diffuse    : 1;  // 0x1
             byte specular   : 1;  // 0x2
             byte normal     : 1;  // 0x4
-        } texture_types;
+        } texture;
     };
 
     class Texture
