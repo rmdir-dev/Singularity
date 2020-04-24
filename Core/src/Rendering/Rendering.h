bf55fcf168dc980b99144a49ef24768ef11f8a4c
rmdir-dev <stonsphilippe@gmail.com>
2020-04-24 21:34:05 +0200

    [RENDERING] add quad and material to rendering.h

--- Core/src/Rendering/Rendering.h
+++ Core/src/Rendering/Rendering.h
@@ -9,9 +9,14 @@
 //MODELS
 #include "Models/Mesh.h"
 #include "Models/Model.h"
+//#include "Models/Cube.h"
+#include "Models/Quad.h"
 
 //SHADER
 #include "Shaders/Shader.h"
 
 //TEXTURE
 #include "Texture/Texture.h"
+
+//MATERIAL
+#include "Material/Material.h"
