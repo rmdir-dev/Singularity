2b2036514f8aeafe05ac3a074c846d5cf40489ea
rmdir-dev <stonsphilippe@gmail.com>
2020-04-26 16:02:01 +0200

    [CLEANING] clean unused variable.

--- Core/src/Test.cpp
+++ Core/src/Test.cpp
@@ -107,7 +107,6 @@ void Test::ImGUIRenderBeging()
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
-    bool demo = true;
     ImGui::Begin("Debug Window");
     ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 }
