f18dd3a9f84430172bdf0e5c7762948072fba901
rmdir-dev <stonsphilippe@gmail.com>
2020-04-28 11:33:37 +0200

    [LIGHT] clean Directional Light constructors

--- Core/src/Rendering/Lights/DirectionalLight.cpp
+++ Core/src/Rendering/Lights/DirectionalLight.cpp
@@ -6,25 +6,21 @@ namespace Rendering
     DirectionalLight::DirectionalLight()
         : m_Direction(glm::vec3(-0.2f, -1.0f, -0.3f)), Lights()
     {
-        SetupLightBox();
     }
 
     DirectionalLight::DirectionalLight(const glm::vec3& direction, const glm::vec3& color, float intensity)
         : m_Direction(direction), Lights(color, intensity)
     {
-        SetupLightBox();
     }
 
     DirectionalLight::DirectionalLight(const glm::vec3& direction, float intensity)
         : m_Direction(direction), Lights(intensity)
     {
-        SetupLightBox();
     }
 
     DirectionalLight::DirectionalLight(const glm::vec3& direction)
         : m_Direction(direction), Lights()
     {
-        SetupLightBox();
     }
 
     DirectionalLight::~DirectionalLight() 
